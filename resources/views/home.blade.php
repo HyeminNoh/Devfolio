f3cbd123619ce75f81af8d0eb2bfd5a9053d8d97
hyeminnoh <shrmffkfk@naver.com>
2020-07-16 14:58:51 +0900

    [Update] 로그인 후 출력될 프로필 상세 페이지 레이아웃 구현

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,99 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Dashboard') }}</div>
-
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-6 col-md-4">
+                <div class="card">
+                    <div class="card-body">
+                        <div>
+                            <img src="{{ auth()->user()->avatar }}" width="100%;">
                         </div>
-                    @endif
+                        <div style="margin-top: 1em;">
+                            <h3>
+                                {{ auth()->user()->login_name }}
+                            </h3>
+                        </div>
+                    </div>
+                    <div class="card-footer" style="text-align: right">
+                        <a style="color: gray" href={{ "https://github.com/".auth()->user()->login_name }}>Github <i class="fab fa-github"></i></a>
+                    </div>
+                </div>
+            </div>
+            <div class="col-12 col-md-8">
+                <div class="row">
+                    <div class="col">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>📅 Contribution Callendar</h4>
+                            </div>
+                            <div class="card-body">
+                                @if (session('status'))
+                                    <div class="alert alert-success" role="alert">
+                                        {{ session('status') }}
+                                    </div>
+                                @endif
 
-                    {{ __('You are logged in!') }}
+                                {{ __('You are logged in!') }}
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col" style="margin-top:1em;">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>📊 Skills</h4>
+                            </div>
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col">
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <p>Pie Graph</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col">
+                                        <p>Programming Language Skills Statistics</p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col" style="margin-top:1em;">
+                        <div class="card">
+                            <div class="card-header">
+                                <h4>📌 Pinned Repository</h4>
+                            </div>
+                            <div class="card-body">
+                                <div class="row">
+                                    <div class="col-lg-6 col-md-12" style="padding: 1em;">
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <h5>Pinned Repository Name</h5>
+                                                <hr>
+                                                <p>description</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div class="col-lg-6 col-md-12" style="padding: 1em;">
+                                        <div class="card">
+                                            <div class="card-body">
+                                                <h5>Pinned Repository Name</h5>
+                                                <hr>
+                                                <p>description</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
