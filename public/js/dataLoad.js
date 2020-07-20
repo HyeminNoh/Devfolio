b49596f81b643411f09ac19428bb124d324ce3b5
hyeminnoh <shrmffkfk@naver.com>
2020-07-20 17:35:55 +0900

    [Add] Add script for loading and updating contribution calendar data

---
+++ public/js/dataLoad.js
@@ -0,0 +1,45 @@
+window.onload = function () {
+    calendarLoad()
+}
+
+function calendarUpdate() {
+    // 데이터 로드
+    $.ajax({
+        url: "http://127.0.0.1:8000/contributions/update",
+        method: "GET",
+        dataType: "json"
+    }).always(function () {
+        calendarLoad()
+    })
+}
+
+function calendarLoad() {
+    // 데이터 로드
+    $.ajax({
+        url: "http://127.0.0.1:8000/contributions/show",
+        method: "GET",
+        dataType: "json"
+    }).done(function (result) {
+        $('div#calendar-div').empty();
+        // calendar view 자리 다시 그리기
+        drawCalendar(result);
+    }).fail(function () {
+        $('div#calendar-div').empty();
+        const calendarDiv = document.getElementById('calendar-div')
+        const failTxt = document.createElement("h4")
+        failTxt.innerText="데이터가 없습니다."
+        calendarDiv.append(failTxt)
+    })
+}
+
+function drawCalendar(data) {
+    const calendarDiv = document.getElementById('calendar-div')
+    const totalContribution = document.createElement("p")
+    console.log(JSON.parse(data[0].data));
+    const contributions = JSON.parse(data[0].data);
+    totalContribution.innerText = "Total Contribution: "+contributions.totalContributions
+    const updatedDate  = document.createElement('p');
+    updatedDate.innerText = "Last Updated time: "+data[0].updated_dt
+    calendarDiv.append(totalContribution)
+    calendarDiv.append(updatedDate)
+}
