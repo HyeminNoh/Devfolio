eedd1b81cf3fffb48fc0abc6934853e5b85b1f04
hyeminnoh <shrmffkfk@naver.com>
2020-07-20 17:34:57 +0900

    [Update] Add function for updating, storing and showing contribution data

--- app/Http/Controllers/ContributionController.php
+++ app/Http/Controllers/ContributionController.php
@@ -2,33 +2,101 @@
 
 namespace App\Http\Controllers;
 
+use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ContributionController extends Controller
 {
+    public function getData(){
+        // 사용자 토큰 활용 api call
+        $endpoint = "https://api.github.com/graphql";
+        $authToken = auth()->user()->access_token;
+        $query = 'query {
+                    user(login: "'.auth()->user()->github_id.'") {
+                        contributionsCollection {
+                            contributionCalendar {
+                                colors
+                                totalContributions
+                                weeks {
+                                    contributionDays {
+                                      color
+                                      contributionCount
+                                      date
+                                      weekday
+                                    }
+                                    firstDay
+                                }
+                            }
+                        }
+                      }
+                }';
+
+        $client = new \GuzzleHttp\Client();
+        $response = $client->request(
+            'post',
+            $endpoint,
+            [
+                'headers'=>[
+                    'Authorization' => "Bearer {$authToken}",
+                    'Accept' => 'application/json'
+                ],
+                'json'=>[
+                    'query' => $query,
+                ]
+            ]
+        )->getBody();
+
+        // \Log::info(sprintf($response));
+
+        // 데이터 파싱
+        $response_array = json_decode($response);
+        $response = $response_array->data->user->contributionsCollection->contributionCalendar;
+
+        return json_encode($response);
+    }
     /**
      * Store a newly created resource in storage.
      *
-     * @return \Illuminate\Http\Response
+     * @return void
+     * @throws \GuzzleHttp\Exception\GuzzleException
      */
     public function store()
     {
-        // 사용자 토큰 활용 api call
+        $response = $this->getData();
+        DB::table('contributions')->insert([
+            'user_idx'=>auth()->user()->id ,
+            'data'=>$response,
+            'created_dt'=>now(),
+            'updated_dt'=>now()
+        ]);
+    }
 
-        // 데이터 파싱
-
-        // 데이터 적재
+    public function update()
+    {
+        $response = $this->getData();
+        DB::table('contributions')->update(['data'=>$response,'updated_dt'=>now()]);
     }
 
     /**
      * Display the specified resource.
      *
-     * @return \Illuminate\Http\Response
+     * @return string
      */
     public function show()
     {
         // 사용자 아이디 기반 조회
-        $user_idx = auth()->user()->id;
+        $check_data = \App\Contribution::where('user_idx', auth()->user()->id)->first();
+
+        if(! $check_data){
+            $this->store();
+        }
         // select * from contributions where user_idx = $user_idx
+        $user_data = DB::table('contributions')
+            ->select(['data','updated_dt'])
+            ->where('user_idx', auth()->user()->id)
+            ->get()->toJson();
+
+        return $user_data;
     }
 }
