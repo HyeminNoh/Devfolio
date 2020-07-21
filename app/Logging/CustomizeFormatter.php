7b218c78ebdc1e2eac81a68ad2fd9ad517594276
hyeminnoh <shrmffkfk@naver.com>
2020-07-21 10:30:54 +0900

    [Add] Log message format

---
+++ app/Logging/CustomizeFormatter.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Logging;
+
+use Monolog\Formatter\LineFormatter;
+
+class CustomizeFormatter
+{
+    /**
+     * Customize the given logger instance.
+     *
+     * @param  \Illuminate\Log\Logger  $logger
+     * @return void
+     */
+    public function __invoke($logger)
+    {
+        foreach ($logger->getHandlers() as $handler) {
+            $handler->setFormatter(new LineFormatter(
+                "[%datetime%] %level_name% - %message% \n",
+                "Y-m-d H:i:s"
+            ));
+        }
+    }
+}
