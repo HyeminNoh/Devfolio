a4de859ca0105ac8c6f5753d98f49ebee030db10
hyeminnoh <shrmffkfk@naver.com>
2020-07-21 10:31:39 +0900

    [Update] Edit logging config

--- config/logging.php
+++ config/logging.php
@@ -35,69 +35,25 @@ return [
     */
 
     'channels' => [
+        // 다중 채널을 생성할 수 있는 채널
         'stack' => [
             'driver' => 'stack',
-            'channels' => ['single'],
+            'channels' => ['single', 'syslog'],
             'ignore_exceptions' => false,
         ],
 
         'single' => [
             'driver' => 'single',
-            'path' => storage_path('logs/laravel.log'),
-            'level' => 'debug',
-        ],
-
-        'daily' => [
-            'driver' => 'daily',
-            'path' => storage_path('logs/laravel.log'),
-            'level' => 'debug',
-            'days' => 14,
-        ],
-
-        'slack' => [
-            'driver' => 'slack',
-            'url' => env('LOG_SLACK_WEBHOOK_URL'),
-            'username' => 'Laravel Log',
-            'emoji' => ':boom:',
-            'level' => 'critical',
-        ],
-
-        'papertrail' => [
-            'driver' => 'monolog',
-            'level' => 'debug',
-            'handler' => SyslogUdpHandler::class,
-            'handler_with' => [
-                'host' => env('PAPERTRAIL_URL'),
-                'port' => env('PAPERTRAIL_PORT'),
-            ],
-        ],
-
-        'stderr' => [
-            'driver' => 'monolog',
-            'handler' => StreamHandler::class,
-            'formatter' => env('LOG_STDERR_FORMATTER'),
-            'with' => [
-                'stream' => 'php://stderr',
-            ],
+            'tap' => [App\Logging\CustomizeFormatter::class],
+            'path' => storage_path('logs/info/'.date('Y-m-d').'.log'),
+            'level' => 'info',
         ],
 
         'syslog' => [
             'driver' => 'syslog',
+            'tap' => [App\Logging\CustomizeFormatter::class],
+            'path' => storage_path('logs/debug/'.date('Y-m-d').'.log'),
             'level' => 'debug',
-        ],
-
-        'errorlog' => [
-            'driver' => 'errorlog',
-            'level' => 'debug',
-        ],
-
-        'null' => [
-            'driver' => 'monolog',
-            'handler' => NullHandler::class,
-        ],
-
-        'emergency' => [
-            'path' => storage_path('logs/laravel.log'),
         ],
     ],
 
