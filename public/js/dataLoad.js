74c678f20bdf1880f994e267826957f22f40a666
hyeminnoh <shrmffkfk@naver.com>
2020-07-23 16:42:02 +0900

    [Update] Draw heatmap calendar

--- public/js/dataLoad.js
+++ public/js/dataLoad.js
@@ -24,12 +24,12 @@ function calendarLoad() {
         method: "GET",
         dataType: "json"
     }).done(function (result) {
-        $('div#calendar-div').empty();
+        $('div#cal-div').empty();
         // calendar view 자리 다시 그리기
         drawCalendar(result);
     }).fail(function () {
-        $('div#calendar-div').empty();
-        const calendarDiv = document.getElementById('calendar-div')
+        $('div#cal-div').empty();
+        const calendarDiv = document.getElementById('cal-div')
         const failTxt = document.createElement("h4")
         failTxt.innerText="데이터로드에 실패했습니다."
         calendarDiv.append(failTxt)
@@ -37,13 +37,101 @@ function calendarLoad() {
 }
 
 function drawCalendar(data) {
-    const calendarDiv = document.getElementById('calendar-div')
-
-    // console.log(JSON.parse(data[0].data));
-    const contributions = JSON.parse(data[0].data);
-
+    const contributions = JSON.parse(data[0].data)
+    // console.log(contributions)
     document.getElementById('total-contribution-text').innerHTML="Total Contribution: "+contributions.totalContributions
     document.getElementById('calendar-updated-text').innerHTML="Last Updated: "+data[0].updated_dt
+
+    const calDiv = document.getElementById('cal-div')
+    const calRow = document.createElement('div')
+    calRow.className="row"
+    calRow.style.verticalAlign="center"
+
+    // 이전, 다음 버튼
+    const prevBtnCol = document.createElement('div')
+    prevBtnCol.className="col-auto"
+    const nextBtnCol = document.createElement('div')
+    nextBtnCol.className="col-auto"
+
+    const prevBtn = document.createElement('button')
+    const nextBtn = document.createElement('button')
+    prevBtn.className="btn btn-light"
+    nextBtn.className="btn btn-light"
+    prevBtn.style.height="80%"
+    nextBtn.style.height="80%"
+    prevBtn.id = "prev-btn"
+    nextBtn.id = "next-btn"
+    prevBtn.innerText="<"
+    nextBtn.innerText=">"
+
+    prevBtnCol.append(prevBtn)
+    nextBtnCol.append(nextBtn)
+
+    // heatmap이 들어갈 div
+    const heatmapCol = document.createElement('div')
+    heatmapCol.className="col-auto"
+    const calHeatMap = document.createElement('div')
+    calHeatMap.id="cal-heatmap"
+    heatmapCol.append(calHeatMap)
+
+    // heatmap click 시 보일 텍스트 div
+    const calDescription = document.createElement('div')
+    calDescription.id = "onClick-placeholder"
+    calDescription.className = "col-auto"
+    calDescription.style.textAlign="right"
+    calDescription.innerHTML="<p>Click Your Calendar</p>"
+
+    calRow.append(prevBtnCol)
+    calRow.append(heatmapCol)
+    calRow.append(calDescription)
+    calRow.append(nextBtnCol)
+    calDiv.append(calRow)
+
+    // 차트에 사용되는 데이터
+    const dailyData = contributions.dailyData
+    const dataKeyList = Object.keys(dailyData)
+    const startUnixTime = dataKeyList[0]
+    const minDate = new Date(new Date(startUnixTime*1000).getFullYear(), new Date(startUnixTime*1000).getMonth())
+    const lastUnixTime = dataKeyList[dataKeyList.length-1]
+    const maxDate = new Date(new Date(lastUnixTime*1000).getFullYear(), new Date(lastUnixTime*1000).getMonth())
+    const startDate = new Date(new Date(lastUnixTime*1000).getFullYear(), new Date(lastUnixTime*1000).getMonth()-6)
+    const colors = contributions.colors
+
+    const cal = new CalHeatMap();
+    cal.init({
+        start: startDate,
+        minDate: minDate,
+        maxDate: maxDate,
+        data: dailyData,
+        domain: "month",
+        subDomain: "day",
+        range: 6,
+        legend: [1, 3, 5, 10],
+        legendColors: ["#efefef"].concat(colors[colors.length-2]),
+        legendHorizontalPosition: "right",
+        nextSelector: "#next-btn",
+        previousSelector: "#prev-btn",
+        onClick: function(date, cnt) {
+            $("#onClick-placeholder").html(
+                date.toDateString() + "</b> <br/>with <b>" +
+                (cnt === null ? "0" : cnt) + "</b> contributions"
+            );
+        }
+    });
+
+    $("#prev-btn").on("click", function(e) {
+        e.preventDefault();
+        if (!cal.previous()) {
+            return false
+        }
+    });
+
+    $("#next-btn").on("click", function(e) {
+        e.preventDefault();
+        if (!cal.next()) {
+            return false
+        }
+    });
 }
 
 /* 대표 저장소 데이터 로드 & 카드 뷰 생성 */
