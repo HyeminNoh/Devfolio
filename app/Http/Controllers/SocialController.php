ba2cfb364fae15807fd6aa3c84248afd7560e127
hyeminnoh <shrmffkfk@naver.com>
2020-07-23 10:30:48 +0900

    [Update] Code Refactoring
    Fix code style and Exception logic

--- app/Http/Controllers/SocialController.php
+++ app/Http/Controllers/SocialController.php
@@ -6,7 +6,6 @@ use App\User;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
 use Illuminate\Routing\Redirector;
-use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Log;
 use Laravel\Socialite\Facades\Socialite;
 
@@ -59,20 +58,37 @@ class SocialController extends Controller
             // oauth 정보 로드
             $socialData = Socialite::driver($provider)->user();
 
-            // 사용자 존재 체크
-            $user = (User::whereEmail($socialData->getEmail())->first());
+            // 필수 정보 조회 성공 여부 확인
+            if(empty($socialData->token)){
+                Log::info('Loading'.$provider.' access token is fail');
+                return redirect('/');
+            }
+
+            if(empty($socialData->getEmail())){
+                Log::info('Loading'.$provider.' user email is fail');
+                return redirect('/');
+            }
+
+            // 중복 사용 되는 값 변수 처리
+            $userMail = $socialData->getEmail();
+            $userNickname = $socialData->getNickname();
+            $nowDt = now();
+
+            // 사용자 등록 여부 확인
+            $user = (User::whereEmail($userMail)->first());
+
+            // 새로운 사용자 추가
             if(! $user) {
-                // 새로운 사용자 추가
                 $user = User::create([
-                    'name' => $socialData->getName() ?: $socialData->getNickname(),
-                    'email' => $socialData->getEmail(),
-                    'github_id' => $socialData->getNickname(),
+                    'name' => (!empty($socialData->getName())) ?: $userNickname,
+                    'email' => $userMail,
+                    'github_id' => $userNickname,
                     'access_token' => $socialData->token,
-                    'github_url' => $socialData->user['html_url'],
-                    'blog_url' => $socialData->user['blog'] ?: '',
-                    'avatar' => $socialData->getAvatar(),
-                    'updated_dt' => now(),
-                    'created_dt' => now()
+                    'github_url' => (!empty($socialData->user['html_url'])) ?: '',
+                    'blog_url' => (!empty($socialData->user['blog'])) ?: '',
+                    'avatar' => (!empty($socialData->getAvatar())) ?: '',
+                    'updated_dt' => $nowDt,
+                    'created_dt' => $nowDt
                 ]);
                 Log::info('Sign Up: '.$socialData->getEmail());
             }
