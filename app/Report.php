c683cccfaf3afa5a59fc4ea6f9d03786cd3ac986
hyeminnoh <shrmffkfk@naver.com>
2020-07-24 14:31:29 +0900

    [Add] Add report model file

--- app/Report.php
+++ app/Report.php
@@ -4,12 +4,12 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class Contribution extends Model
+class Report extends Model
 {
     public $timestamps = false;
     protected $primaryKey = 'idx';
 
-    protected $fillable = ['user_idx', 'data'];
+    protected $fillable = ['user_idx', 'type', 'data'];
 
     public function user(){
         return $this->belongsTo(User::class);
