46cb8bae7ae39fa421139627a2c1f640a545b439
hyeminnoh <shrmffkfk@naver.com>
2020-07-27 16:35:40 +0900

    [Update] Register repositories

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Repositories\ReportRepositoryInterface;
+use App\Repositories\ReportRepository;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -13,7 +15,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        //
+        $this->app->bind(
+            ReportRepositoryInterface::class,
+            ReportRepository::class
+        );
     }
 
     /**
