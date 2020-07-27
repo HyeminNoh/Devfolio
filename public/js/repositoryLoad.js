29c7017f50f98d15f2144b2de91a3623352e55c0
hyeminnoh <shrmffkfk@naver.com>
2020-07-27 18:01:15 +0900

    [Update] Add modals
    Card view 클릭 시 보일 Modal 생성

--- public/js/repositoryLoad.js
+++ public/js/repositoryLoad.js
@@ -9,7 +9,7 @@ function repositoriesLoad() {
     }).done(function (result) {
         $('div#repositories-div').empty();
         // calendar view 자리 다시 그리기
-        drawRepositCard(result);
+        drawRepositCards(result);
     }).fail(function () {
         $('div#repositories-div').empty();
         const repositDiv = document.getElementById('repositories-div')
@@ -19,7 +19,18 @@ function repositoriesLoad() {
     })
 }
 
-function drawRepositCard(data) {
+function repositoriesUpdate() {
+    // 데이터 갱신
+    $.ajax({
+        url: "http://127.0.0.1:8000/report/"+this.userIdx+"/repository/update",
+        method: "GET",
+        dataType: "json"
+    }).always(function () {
+        repositoriesLoad()
+    })
+}
+
+function drawRepositCards(data) {
     document.getElementById('repository-updated-text').innerHTML="Last Updated: "+data[0].updated_dt
 
     const repositoriesDiv = document.getElementById('repositories-div')
@@ -37,55 +48,8 @@ function drawRepositCard(data) {
             col.className = "col-lg-6 col-md-12"
             col.style.padding = "1em"
 
-            const card = document.createElement('div')
-            card.className = "card"
-
-            const cardBody = document.createElement('div')
-            cardBody.className = "card-body"
-
-            const title = document.createElement('div')
-            const description = document.createElement('div')
-            const stat = document.createElement('div')
-            stat.className="row"
-            const leftRow = document.createElement('div')
-            leftRow.className="row"
-            const leftCol = document.createElement('div')
-            leftCol.className="col"
-            const rightCol = document.createElement('div')
-            rightCol.className="col"
-            rightCol.style.textAlign="right"
-            const stargazer = document.createElement('col')
-            stargazer.style.marginLeft="1em"
-            const language = document.createElement('col')
-            language.style.marginLeft="0.3em"
-            const fork = document.createElement('col')
-            fork.style.marginLeft="0.3em"
-
-            title.innerHTML="<a href=" + node.url + "><h5>" + node.name + "</h5></a><hr>"
-            description.innerHTML = "<p>"+node.description+"</p>"
-            if(node.stargazers.totalCount!==0){
-                stargazer.innerHTML="<p style='color:#808080;'><i class=\"fas fa-star\" style=\"color: #808080\"></i>&nbsp"+node.stargazers.totalCount+"</p>"
-            }
-            if(node.primaryLanguage) {
-                language.innerHTML = "<p style='color:#808080;'><i class=\"fas fa-circle\" style='color:"+node.primaryLanguage.color+"'></i>&nbsp" + node.primaryLanguage.name + "</p>"
-            }
-            if(node.forkCount!==0) {
-                fork.innerHTML = "<p style='color:#808080;'><i class=\"fas fa-code-branch\" style=\"color: #808080\"></i>&nbsp" + node.stargazers.totalCount + "</p>"
-            }
-
-            rightCol.innerHTML = "<p>"+node.diskUsage+" KB</p>"
-
-            leftRow.append(stargazer)
-            leftRow.append(language)
-            leftRow.append(fork)
-            leftCol.append(leftRow)
-            // 카드 바디 안에 내용 채우기
-            stat.append(leftCol)
-            stat.append(rightCol)
-            cardBody.append(title)
-            cardBody.append(description)
-            cardBody.append(stat)
-            card.append(cardBody)
+            makeModal(node)
+            const card = makeCard(node)
             col.append(card)
             row.append(col)
         })
@@ -100,13 +64,89 @@ function drawRepositCard(data) {
     repositoriesDiv.append(row);
 }
 
-function repositoriesUpdate() {
-    // 데이터 갱신
-    $.ajax({
-        url: "http://127.0.0.1:8000/report/"+this.userIdx+"/repository/update",
-        method: "GET",
-        dataType: "json"
-    }).always(function () {
-        repositoriesLoad()
-    })
+function makeCard(node) {
+    const card = document.createElement('div')
+    card.className = "card"
+
+    const cardBody = document.createElement('div')
+    cardBody.className = "card-body"
+
+    const title = document.createElement('div')
+    const description = document.createElement('div')
+    const stat = document.createElement('div')
+    stat.className="row"
+    const leftRow = document.createElement('div')
+    leftRow.className="row"
+    const leftCol = document.createElement('div')
+    leftCol.className="col"
+    const rightCol = document.createElement('div')
+    rightCol.className="col"
+    rightCol.style.textAlign="right"
+    const stargazer = document.createElement('col')
+    stargazer.style.marginLeft="1em"
+    const language = document.createElement('col')
+    language.style.marginLeft="0.3em"
+    const fork = document.createElement('col')
+    fork.style.marginLeft="0.3em"
+
+    title.innerHTML="<a href=" + node.url + "><h5>" + node.name + "</h5></a><hr>"
+    description.innerHTML = "<p>"+node.description+"</p>"
+    if(node.stargazers.totalCount!==0){
+        stargazer.innerHTML="<p style='color:#808080;'><i class=\"fas fa-star\" style=\"color: #808080\"></i>&nbsp"+node.stargazers.totalCount+"</p>"
+    }
+    if(node.primaryLanguage) {
+        language.innerHTML = "<p style='color:#808080;'><i class=\"fas fa-circle\" style='color:"+node.primaryLanguage.color+"'></i>&nbsp" + node.primaryLanguage.name + "</p>"
+    }
+    if(node.forkCount!==0) {
+        fork.innerHTML = "<p style='color:#808080;'><i class=\"fas fa-code-branch\" style=\"color: #808080\"></i>&nbsp" + node.stargazers.totalCount + "</p>"
+    }
+
+    rightCol.innerHTML = "<p>"+node.diskUsage+" KB</p>"
+
+    const stretchedLink = document.createElement('a')
+    stretchedLink.className='stretched-link'
+    stretchedLink.setAttribute('data-toggle', 'modal')
+    stretchedLink.href="#"+node.name+"Modal"
+
+    leftRow.append(stargazer)
+    leftRow.append(language)
+    leftRow.append(fork)
+    leftCol.append(leftRow)
+    // 카드 바디 안에 내용 채우기
+    stat.append(leftCol)
+    stat.append(rightCol)
+    cardBody.append(title)
+    cardBody.append(description)
+    cardBody.append(stat)
+    cardBody.append(stretchedLink)
+    card.append(cardBody)
+    return card
+}
+
+function makeModal(node) {
+    const modal = document.createElement('div')
+    modal.id = node.name+'Modal'
+    modal.className = 'modal fade'
+    modal.setAttribute('role', 'dialog')
+
+    const dialog = document.createElement('div')
+    dialog.className='modal-dialog'
+
+    const content = document.createElement('div')
+    content.className='modal-content'
+
+    const header = document.createElement('div')
+    header.innerHTML = "<button type='button' class='close' data-dismiss='modal'>&times;</button><h4 class='modal-title'>"+node.name+"</h4>"
+
+    const body = document.createElement('div')
+    body.className='modal-body'
+    body.innerHTML="<p>hi</p>"
+
+    content.append(header)
+    content.append(body)
+    dialog.append(content)
+    modal.append(dialog)
+
+    const container = document.getElementById('main-container')
+    container.append(modal)
 }
--- resources/views/portfolio.blade.php
+++ resources/views/portfolio.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
+    <div class="container" id="main-container">
         <div class="row justify-content-center">
             <div class="col-6 col-md-4">
                 <div class="row">
