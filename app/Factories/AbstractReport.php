96ea7562f49569511355cc74d9ea8961d0086731
hyeminnoh <shrmffkfk@naver.com>
2020-07-27 16:34:39 +0900

    [Add] Add abstract report class

---
+++ app/Factories/AbstractReport.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace App\Factories;
+
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Illuminate\Support\Facades\Log;
+
+abstract class AbstractReport
+{
+    abstract public function getData();
+    abstract public function setData($userIdx);
+    abstract public function parseData($apiResponse);
+    public function callApi($token, $query, $type){
+        $endpoint = "https://api.github.com/graphql";
+        $client = new Client();
+        try {
+            $response = $client->request(
+                'post',
+                $endpoint,
+                [
+                    'headers' => [
+                        'Authorization' => "Bearer {$token}",
+                        'Accept' => 'application/json'
+                    ],
+                    'json' => [
+                        'query' => $query,
+                    ]
+                ]
+            )->getBody();
+
+            Log::info('Calling API for'.$type.'Data Success');
+            return json_decode($response);
+
+        } catch (GuzzleException $e) {
+            // api 오류 처리
+            Log::info("Calling API for".$type."Data Fail");
+            Log::debug("Calling API Error Message: \n" . $e);
+            return false;
+        }
+    }
+}
