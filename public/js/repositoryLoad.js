df220b65b45322252691d17728c30a61dbbadd45
hyeminnoh <shrmffkfk@naver.com>
2020-07-28 17:40:48 +0900

    [Update] Add draw modal function

--- public/js/repositoryLoad.js
+++ public/js/repositoryLoad.js
@@ -40,7 +40,6 @@ function drawRepositCards(data) {
 
     // pinned reposit 정보만 추출
     data = JSON.parse(data[0].data)
-    console.log(data)
     if(data) {
         data.forEach(function (node) {
             const col = document.createElement('div')
@@ -104,7 +103,10 @@ function makeCard(node) {
     const stretchedLink = document.createElement('a')
     stretchedLink.className='stretched-link'
     stretchedLink.setAttribute('data-toggle', 'modal')
-    stretchedLink.href="#repoModal"
+    stretchedLink.setAttribute('data-target', '#repoModal')
+    stretchedLink.onclick = function(){
+        makeModal(node)
+    }
 
     leftRow.append(stargazer)
     leftRow.append(language)
@@ -122,6 +124,166 @@ function makeCard(node) {
 }
 
 function makeModal(node) {
-    console.log(node)
+    // draw modal
+    const title = document.getElementById('repoModalTitle')
+    title.innerText = node.name
+    const titleLink = document.getElementById('titleLink')
+    titleLink.setAttribute('href', node.url)
+    const star = document.getElementById('modalCount')
+    star.innerHTML = "<i class=\"fas fa-star\" style=\"color: #808080\"></i>&nbsp"+node.totalCount+"&nbsp;<i class=\"fas fa-code-branch\" style=\"color: #808080\"></i>&nbsp"+node.forkCount
+    const description = document.getElementById('repoModalDesc')
+    description.innerText = node.description
 
+    $('div#ModalPageUrl').empty()
+    if(node.homepageUrl){
+        const pageUrl = document.getElementById('ModalPageUrl')
+        pageUrl.innerHTML="<p style='overflow: hidden; text-overflow: ellipsis; display: inline-block; width: 100%; white-space: nowrap;'><i class=\"fas fa-link\" style='color:gray;'></i>&nbsp;&nbsp;<a style='color:gray;' href='"+node.homepageUrl+"'>"+node.homepageUrl+"</a></p>"
+    }
+    // 언어 사용 정렬
+    const languages = node.languages.edges
+    languages.sort(function (a, b) {
+        return b.size - a.size;
+    })
+
+    const langLabels = []
+    const langValues = []
+    const langColors = []
+
+    // 언어 사용 설명
+    const langDesc = document.getElementById("modalLangDesc");
+    $('div#modalLangDesc').empty();
+    if(languages.length) {
+        for (let i = 0; i < languages.length; i++) {
+            const langText = document.createElement("p")
+            langText.innerHTML = "<i class='fas fa-circle' style='color:" + languages[i].node.color + " '></i>&nbsp;&nbsp;" + languages[i].node.name + ": " + languages[i].size + " lines"
+            langDesc.append(langText)
+            langLabels.push(languages[i].node.name)
+            langValues.push(languages[i].size)
+            langColors.push(languages[i].node.color)
+
+        }
+    } else {
+        const langText = document.createElement("p")
+        langText.innerHTML = "<i class='fas fa-circle' style='color:gray;'></i>&nbsp;&nbsp;Markdown All files"
+        langDesc.append(langText)
+        langLabels.push('Markdown')
+        langValues.push(1)
+        langColors.push('gray')
+    }
+
+    // 언어 사용 통계 그래프
+    $('div#modalLangChart').empty();
+    const langChartDiv = document.getElementById('modalLangChart')
+    const langChart = document.createElement('canvas')
+    langChart.width=100
+    langChart.height=100
+    const ctx = langChart.getContext('2d');
+    new Chart(ctx, {
+        type: 'doughnut',
+        data: {
+            datasets: [{
+                data: langValues,
+                backgroundColor: langColors,
+            }],
+
+            // These labels appear in the legend and in the tooltips when hovering different arcs
+            labels: langLabels
+        },
+        options: {
+            tooltips: {
+                callbacks: {
+                    label: function(tooltipItem, data) {
+                        //get the concerned dataset
+                        const dataset = data.datasets[tooltipItem.datasetIndex];
+                        //calculate the total of this data set
+                        const total = dataset.data.reduce(function(previousValue, currentValue, currentIndex, array) {
+                            return previousValue + currentValue;
+                        });
+                        //get the current items value
+                        const currentValue = dataset.data[tooltipItem.index];
+                        //calculate the precentage based on the total and current item, also this does a rough rounding to give a whole number
+                        const percentage = Math.floor(((currentValue/total) * 100)+0.5);
+                        return langLabels[tooltipItem.index]+" "+percentage + "%";
+                    }
+                }
+            },
+            legend: {
+                display: false
+            }
+        }
+    });
+    langChartDiv.append(langChart);
+
+    // 기여도 정렬
+    const contributor = node.contributor
+    contributor.sort(function (a, b) {
+        return b.contributions - a.contributions;
+    })
+
+    const contriNames = []
+    const contriValues = []
+
+    const contriCol = document.getElementById('contriProfileCol')
+    $('div#contriProfileCol').empty();
+    for(let i=0; i<contributor.length; i++){
+        const contributorProfile = document.createElement('div')
+        contributorProfile.className="row"
+        contributorProfile.style.marginTop="1em"
+        const profileImg = document.createElement('div')
+        profileImg.className='col'
+        profileImg.innerHTML = "<img src='"+contributor[i].avatar_url+"' class=\"img-thumbnail\"/>"
+
+        const profileDesc = document.createElement('div')
+        profileDesc.className='col'
+        profileDesc.innerHTML="<p><a href="+contributor[i].html_url+"><span style='font-weight: bold'>"+contributor[i].login+"</span></a> "+contributor[i].contributions+" <span style='font-size: small'>contributions</span></p>"
+
+        contributorProfile.append(profileImg)
+        contributorProfile.append(profileDesc)
+        contriCol.append(contributorProfile)
+
+        contriNames.push(contributor[i].login)
+        contriValues.push(contributor[i].contributions)
+    }
+
+    // 언어 사용 통계 그래프
+    $('div#contriChartDiv').empty();
+    const contriChartDiv = document.getElementById('contriChartDiv')
+    const contriChart = document.createElement('canvas')
+    contriChart.width=100
+    contriChart.height=100
+    const ctx2 = contriChart.getContext('2d');
+    new Chart(ctx2, {
+        type: 'doughnut',
+        data: {
+            datasets: [{
+                data: contriValues
+            }],
+
+            // These labels appear in the legend and in the tooltips when hovering different arcs
+            labels: contriNames
+        },
+        options: {
+            tooltips: {
+                callbacks: {
+                    label: function(tooltipItem, data) {
+                        //get the concerned dataset
+                        const dataset = data.datasets[tooltipItem.datasetIndex];
+                        //calculate the total of this data set
+                        const total = dataset.data.reduce(function(previousValue, currentValue, currentIndex, array) {
+                            return previousValue + currentValue;
+                        });
+                        //get the current items value
+                        const currentValue = dataset.data[tooltipItem.index];
+                        //calculate the precentage based on the total and current item, also this does a rough rounding to give a whole number
+                        const percentage = Math.floor(((currentValue/total) * 100)+0.5);
+                        return contriNames[tooltipItem.index]+" "+percentage + "%";
+                    }
+                }
+            },
+            legend: {
+                display: false
+            }
+        }
+    });
+    contriChartDiv.append(contriChart);
 }
