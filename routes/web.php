c088968cbc61f5e14aff5b1fc00339ccabcc4cca
hyeminnoh <shrmffkfk@naver.com>
2020-07-31 10:57:23 +0900

    [Update] Route root view with  users list

--- routes/web.php
+++ routes/web.php
@@ -15,13 +15,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $userList = User::inRandomOrder()->get();
+    return view('welcome', ['userList'=>$userList]);
 })->name('root');
 
 Route::get('portfolio/{userIdx}', function ($userIdx) {
     $user = User::find($userIdx);
     return view('portfolio', ['user' => $user]);
-});
+})->name('portfolio');
 
 /* social login */
 Route::get('social/{provider}', [
