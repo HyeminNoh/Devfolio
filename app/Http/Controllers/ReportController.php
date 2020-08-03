64cb2419194eb84465dabd6808b5f764e51f287a
hyeminnoh <shrmffkfk@naver.com>
2020-08-03 17:15:09 +0900

    [Update] Add warning alert for not founded user

--- app/Http/Controllers/ReportController.php
+++ app/Http/Controllers/ReportController.php
@@ -3,6 +3,12 @@
 namespace App\Http\Controllers;
 
 use App\Repositories\ReportRepository;
+use App\Repositories\UserRepository;
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\View\View;
+use RealRashid\SweetAlert\Facades\Alert;
 
 class ReportController extends Controller
 {
@@ -10,18 +16,36 @@ class ReportController extends Controller
      * @var ReportRepository
      */
     protected $reportRepo;
+    protected $userRepo;
 
     /**
      * ReportController constructor.
      * @param ReportRepository $reportRepo
+     * @param UserRepository $userRepo
      */
-    public function __construct(ReportRepository $reportRepo)
+    public function __construct(ReportRepository $reportRepo, UserRepository $userRepo)
     {
+        $this->userRepo = $userRepo;
         $this->reportRepo = $reportRepo;
     }
 
     /**
-     * Renew user's report data
+     * Show user portfolio view
+     *
+     * @param $githubId
+     * @return Application|Factory|RedirectResponse|View
+     */
+    public function show($githubId){
+        $user = $this->userRepo->getGithub($githubId);
+        if(empty($user)){
+            Alert::warning('Not Found User', 'The user you are looking for was not found');
+            return redirect()->back();
+        }
+        return view('portfolio', ['user' => $user]);
+    }
+
+    /**
+     * Renew user report data
      *
      * @param $userIdx
      * @param string $type
@@ -33,13 +57,13 @@ class ReportController extends Controller
     }
 
     /**
-     * Returing user's report data
+     * Return user report data
      *
      * @param $userIdx
      * @param string $type
      * @return string
      */
-    public function show($userIdx, $type)
+    public function get($userIdx, $type)
     {
         return $this->reportRepo->get($userIdx, $type);
     }
